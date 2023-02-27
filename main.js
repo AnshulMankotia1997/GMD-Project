7938fbf031645e7bd04c4c9933e2bfdeeed7fa92
Anshul Mankotia <anshulmankotia1997@gmail.com>
2023-02-27 10:17:06 +0530

    add back to top button and add js for it also css

---
+++ main.js
@@ -0,0 +1,10 @@
+// for back to top button
+const toTop = document.querySelector(".top");
+
+window.addEventListener("scroll" , ()=>{
+    if(window.pageYOffset >100){
+        toTop.classList.add("active")
+    }else{
+     toTop.classList.remove("active")   
+    }
+})
